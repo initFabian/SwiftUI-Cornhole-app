3afea81fc6c27c1bbc1978234754f2c933f5c6af
Fabian Buentello <fabian.buentello@gmail.com>
2019-06-23 11:27:45 -0500

    New master (#1)
    * initial commit
    
    * fix code
    
    * new master

---
+++ Cornhole App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Cornhole App
+//
+//  Created by Fabian Catalina on 6/23/19.
+//  Copyright © 2019 Fabian Buentello. All rights reserved.c//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello World")
+  }
+}
+
+#if DEBUG
+  struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+      ContentView()
+    }
+  }
+#endif
